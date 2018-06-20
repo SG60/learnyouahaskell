fca93e95dff1be75db865d9c7457da860ed0de3f
SG60 <samjg60@gmail.com>
2018-06-20 20:17:32 +0100

    People

--- People.hs
+++ People.hs
@@ -16,3 +16,18 @@ data Car = Car
 tellCar :: Car -> String
 tellCar (Car {company = c, model = m, year = y}) =
   "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+data Vector a =
+  Vector a
+         a
+         a
+  deriving (Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i + l) (j + m) (k + n)
+
+vmult :: (Num t) => Vector t -> t -> Vector t
+(Vector i j k) `vmult` m = Vector (i * m) (j * m) (k * m)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `scalarMult` (Vector l m n) = Vector (i * l) (j * m) (k * n)
